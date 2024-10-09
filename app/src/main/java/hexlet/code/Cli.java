1b5f2c5f75aeaf219666fc7e78bc59431bd26887
Dangerwind <dangerwind@gmail.com>
2024-10-09 21:18:28 +0300

    static Cli file

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static String getUserName() {
+    public static void greet() {
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
         scanner.close();
-        return userName;
     }
 
 }
